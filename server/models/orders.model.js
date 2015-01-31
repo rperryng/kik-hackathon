a68f214f365e02eb9ff48539725c7eda22e84527
oliviakim95 <oliviakim321@gmail.com>
2015-01-31 12:05:23 -0500

    Completed orders model

--- server/models/orders.model.js
+++ server/models/orders.model.js
@@ -3,5 +3,24 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var orderSchema = new Schema({
+
+  kikUsername: String,
+  fullName: String,
+  orderType: Number,
+  pizzas: [{
+  	size: String,
+  	toppings: [String],
+  	price: Number
+  }],
+  created: {
+    type: Date,
+    default: Date.now
+  }
+  dueTime: {
+    type: Date,
+    default: Date.now
+  } 
+  address: String,
+  confirmationNum: Number,
   
 });
